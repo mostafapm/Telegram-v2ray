f8c6c43b9d56269354560d43e397ef2e79adb624
mostafapm <mostafapm48@gmail.com>
2025-07-26 17:17:07 +0330

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام خوبی؟ 😊")
 
 if __name__ == '__main__':
-    TOKEN = "توکن_ربات_تو_اینجا_بذار"
+    TOKEN = "7831184685:AAGYVXKQ1lorzH9XmoosYtv8s07F1ioqCQo"
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
